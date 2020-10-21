cd668a8bdbebd335014af3c76c896f41d62908d6
Andrew Harrington <ah4axr@virginia.edu>
2020-10-20 23:12:39 -0400

    Initial Commit Simple Clock Mobile App

---
+++ App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Clock  from './Components/Clock'
+import Constants from 'expo-constants';
+
+
+export default class App extends React.Component {  
+    render(){
+      return (<Clock/>)
+    }
+
+  }
+
